7288ff74661e6c8a8b5463c93bb29a4384062bac
Alexis Winters <alexiscodes@yahoo.com>
2023-06-15 08:20:15 -0500

    installed mui, added css, about section done

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,7 +1,43 @@
+// import "../css/AboutSection.css"
+import LinkedInIcon from "@mui/icons-material/LinkedIn";import GitHubIcon from "@mui/icons-material/GitHub";
+import "../App.css"
+
 const AboutSection = () => {
   return (
-    <div>AboutSection</div>
-  )
-}
+    <section className="home" id="home">
+      <div className="home-container">
+        <div className="bio">
+          <h3>Hello, my name is</h3>
+          <h1>Alexis Winters,</h1>
+          <h3>
+            and I'm a <span id="typed-strings">Full Stack Developer.</span>
+          </h3>
+          <p>
+            ...and I like to build things on the web. I'm a full-stack software developer with a passion for front-end development. I enjoy crafting
+            interactive and responsive websites that leave a lasting impression. Take a look at my portfolio and let's bring your ideas to life
+            together!
+          </p>
+          <div className="social-media">
+            <a href="https://www.linkedin.com/in/alexis-winters/" target="_blank">
+              <LinkedInIcon />
+            </a>
+            {/* <a href="https://www.instagram.com/alexisnicole.dev/" target="_blank">
+              <i className="fa fa-instagram"></i>
+            </a> */}
+            <a href="https://github.com/alexiscodes21/" target="_blank">
+              <GitHubIcon />
+            </a>
+          </div>
+          <a href="../pdf/Resume_AlexisWinters.pdf" target="_blank" className="btn">
+            Resume
+          </a>
+        </div>
+        {/* <div className="home-img">
+          <img className="alexis" src="img/photo4.jpeg" alt="Photo of Alexis" />
+        </div> */}
+      </div>
+    </section>
+  );
+};
 
-export default AboutSection
+export default AboutSection;
